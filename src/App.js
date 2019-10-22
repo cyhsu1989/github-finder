2cfe44a790bec57c0670dd166e4730b69baa47e4
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-22 19:27:27 +0800

    [先利用假資料畫出多個 User 卡片資訊] 新增 UserItem component * 先做出 User Item 卡片資訊。
    新增 Users component
    * 新增一組 users 陣列，做假資料。
    * 利用 map 去跑每一筆資料，並同時將資料傳進 UserItem 組件。#

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./component/layout/Navbar";
+import Users from "./component/user/Users";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Navbar></Navbar>
+				<div className="container">
+					<Users></Users>
+				</div>
 			</div>
 		);
 	}
