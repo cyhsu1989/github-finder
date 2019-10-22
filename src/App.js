0327e85c89c5c45381239af1418332ca1d5dba4d
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-22 21:48:48 +0800

    [HTTP Requests & Updating State] 使用 axios 抓取 API。

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./component/layout/Navbar";
 import Users from "./component/user/Users";
+import axios from "axios";
 
 class App extends Component {
+	state = {
+		users: []
+	};
+
+	componentDidMount() {
+		axios.get("https://api.github.com/users").then(res => {
+			this.setState({ users: res.data });
+		});
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<Navbar></Navbar>
 				<div className="container">
-					<Users></Users>
+					<Users users={this.state.users}></Users>
 				</div>
 			</div>
 		);
