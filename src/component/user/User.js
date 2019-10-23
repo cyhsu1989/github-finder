f7b518cf322a9de1992e1396d186dcec11605b26
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-23 20:07:30 +0800

    [User Component UI & Layout] - 根據 props 中的 loading 判斷是否要顯示 Spinner component。 - 增加 Back To Search 按鈕，回到首頁。 - 將取回來的資料 Binding 至 UI 及 Layout。 -

--- src/component/user/User.js
+++ src/component/user/User.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
 
 class User extends Component {
 	componentDidMount() {
@@ -7,6 +9,7 @@ class User extends Component {
 	render() {
 		const {
 			name,
+			company,
 			avatar_url,
 			location,
 			bio,
@@ -20,7 +23,82 @@ class User extends Component {
 			hireable
 		} = this.props.user;
 		const { loading } = this.props;
-		return <div>{name}</div>;
+
+		if (loading) return <Spinner></Spinner>;
+
+		return (
+			<Fragment>
+				<Link to="/" className="btn btn-light">
+					Back To Search
+				</Link>
+				Hireable:{" "}
+				{hireable ? (
+					<i className="fa fa-check text-sucess"></i>
+				) : (
+					<i className="fa fa-times-circle text-danger"></i>
+				)}
+				<div className="card grid-2">
+					<div className="all-center">
+						<img
+							src={avatar_url}
+							className="round-img"
+							alt="avatar"
+							style={{ width: "150px" }}
+						/>
+						<h1>{name}</h1>
+						<p>Location: {location}</p>
+					</div>
+					<div>
+						{bio && (
+							<Fragment>
+								<h3>Bio</h3>
+								<p>{bio}</p>
+							</Fragment>
+						)}
+						<a href={html_url} className="btn btn-dark m-1">
+							Visit GitHub Profile
+						</a>
+						<ul>
+							<li>
+								{login && (
+									<Fragment>
+										<strong>Username: </strong> {login}
+									</Fragment>
+								)}
+							</li>
+							<li>
+								{company && (
+									<Fragment>
+										<strong>Company: </strong> {company}
+									</Fragment>
+								)}
+							</li>
+							<li>
+								{blog && (
+									<Fragment>
+										<strong>Website: </strong> {blog}
+									</Fragment>
+								)}
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className="card text-center">
+					<div className="badge badge-primary">
+						Followers: {followers}
+					</div>
+					<div className="badge badge-success">
+						Following: {following}
+					</div>
+					<div className="badge badge-light">
+						Public Repos: {public_repos}
+					</div>
+					<div className="badge badge-dark">
+						Public Gists: {public_gists}
+					</div>
+				</div>
+			</Fragment>
+		);
 	}
 }
 
