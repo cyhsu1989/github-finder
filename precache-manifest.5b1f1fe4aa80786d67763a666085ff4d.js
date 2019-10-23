804e9c483e479d2bfe76c1ea4de8c374d34c3f27
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-24 01:00:03 +0800

    Updates

---
+++ precache-manifest.5b1f1fe4aa80786d67763a666085ff4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1240bb9fda676bd9aa1b1cc5336151b4",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "8dbab452bff9bc266ebf",
+    "url": "/github-finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "c3a1523fcde692a3b80c",
+    "url": "/github-finder/static/js/2.5fdf5344.chunk.js"
+  },
+  {
+    "revision": "8dbab452bff9bc266ebf",
+    "url": "/github-finder/static/js/main.4fdc73de.chunk.js"
+  },
+  {
+    "revision": "5f1f259c977a7c780146",
+    "url": "/github-finder/static/js/runtime-main.b6e0201c.js"
+  }
+]);
