790467794fa975b5014c65bda4ed07503cbe7d31
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-23 15:44:18 +0800

    [Passing Props Up & Search] - 從上層組件給子組件函式當作 props 傳入，子組件就可以呼叫，將值傳遞進去，呼叫 Search API 取值。 - 新增 PropTypes 檢查。

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./component/layout/Navbar";
 import Users from "./component/user/Users";
+import Search from "./component/user/Search";
 import axios from "axios";
 
 class App extends Component {
@@ -21,12 +22,24 @@ class App extends Component {
 			});
 	}
 
+	searchUsers = text => {
+		this.setState({ loading: true });
+		axios
+			.get(
+				`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secrect=${process.env.REACT_APP_GITHUB_CLIENT_SECRECT}`
+			)
+			.then(res => {
+				this.setState({ users: res.data.items, loading: false });
+			});
+	};
+
 	render() {
 		const { users, loading } = this.state;
 		return (
 			<div className="App">
 				<Navbar></Navbar>
 				<div className="container">
+					<Search searchUsers={this.searchUsers}></Search>
 					<Users users={users} loading={loading}></Users>
 				</div>
 			</div>
