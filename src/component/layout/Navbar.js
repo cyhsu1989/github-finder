a5010374feec88ac87aa812a00c110d20165ee78
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-23 17:58:35 +0800

    調整 Navbar logo 與文字間距

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = ({ icon, title }) => {
 	return (
 		<nav className="navbar bg-primary">
 			<h1>
-				<i className={icon}></i>
+				<i className={icon} style={{ marginRight: "10px" }}></i>
 				{title}
 			</h1>
 			<ul>
