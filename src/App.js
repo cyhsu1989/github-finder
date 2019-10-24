daf3d7b735fa279f6d62ab5390a71bd3573cbecc
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-24 21:17:46 +0800

    [Alert Context Workflow] 將 Alert 功能使用另外一個 context 管理。

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./component/layout/Navbar";
@@ -9,52 +9,45 @@ import Alert from "./component/layout/Alert";
 import About from "./component/pages/About";
 
 import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
 
 const App = () => {
-	const [alert, setAlert] = useState(null);
-
 	// TODO: 初始化時，就先 search users
-
-	const showAlert = (msg, type) => {
-		setAlert({ msg, type });
-
-		setTimeout(() => {
-			setAlert(null);
-		}, 3000);
-	};
 
 	return (
 		<GithubState>
-			<Router basename={process.env.PUBLIC_URL}>
-				<div className="App">
-					<Navbar></Navbar>
-					<div className="container">
-						<Alert alert={alert}></Alert>
-						<Switch>
-							<Route
-								exact
-								path="/"
-								render={props => (
-									<Fragment>
-										<Search setAlert={showAlert}></Search>
-										<Users></Users>
-									</Fragment>
-								)}
-							></Route>
-							<Route
-								exact
-								path="/about"
-								component={About}
-							></Route>
-							<Route
-								exact
-								path="/user/:login"
-								component={User}
-							></Route>
-						</Switch>
+			<AlertState>
+				<Router basename={process.env.PUBLIC_URL}>
+					<div className="App">
+						<Navbar></Navbar>
+						<div className="container">
+							<Alert></Alert>
+							<Switch>
+								<Route
+									exact
+									path="/"
+									render={props => (
+										<Fragment>
+											<Search></Search>
+											<Users></Users>
+										</Fragment>
+									)}
+								></Route>
+								<Route
+									exact
+									path="/about"
+									component={About}
+								></Route>
+								<Route
+									exact
+									path="/user/:login"
+									component={User}
+								></Route>
+							</Switch>
+						</div>
 					</div>
-				</div>
-			</Router>
+				</Router>
+			</AlertState>
 		</GithubState>
 	);
 };
